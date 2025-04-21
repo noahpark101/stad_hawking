8ef0b90dfe8ab1e8425c2c45873483cfa2b183af
Jianwei Chen <98350779+jchen362@users.noreply.github.com>
2025-04-21 13:10:52 -0400

    edited pom.xml to include jqwik

--- src/test/java/com/zoho/hawking/PropertyTest.java
+++ src/test/java/com/zoho/hawking/PropertyTest.java
@@ -1,4 +1,34 @@
 package com.zoho.hawking;
 
+import com.zoho.hawking.datetimeparser.configuration.HawkingConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class PropertyTest {
+  private HawkingConfiguration hawkingConfiguration;
+  private HawkingTimeParser parser;
+  private Date referenceDate;
+  private String defaultStartDate;
+
+  /**
+   * Gets the date given a Start or End line
+   * @param str the string in the following format: "Start/End : {Date}:{Time}"
+   * @return the Date
+   */
+  private String getDate(String str) {
+    List<String> ls = Arrays.asList(str.split("T"));
+    return ls.get(0);
+  }
+
+  @BeforeEach
+  void setUp() {
+    hawkingConfiguration = new HawkingConfiguration();
+    parser = new HawkingTimeParser();
+    referenceDate = new Date(120, Calendar.DECEMBER, 1);
+    defaultStartDate = "2020-12-01";
+  }
 }
